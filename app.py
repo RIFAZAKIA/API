0f32f0556cd05127d0ca5d1484cace743c7ace37
RIFAZAKIA <rifazakia96@gmail.com>
2020-04-14 15:57:57 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,redirect,
+from flask_mysqldb import MYSQL
+import yaml
+app = Flask(__name__)
+#configaration db
+db=yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_dbt']
+
+    mysql=MYSQL(app)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+   if request.method == 'POST':
+
+    #fetch data
+   userDetails = request.form
+    name =userDetails['name']
+
+
+cur = mysql.connection.cursor()
+cur.execue("INSERT INTO flaskapp(books)VALUES(%5)"), (book)
+mysql.connection.commit()
+cur.close()
+return redirect('/users')
+
+return render_template('index.html')
+
+@app route('/books')
+cur = mysql.connection.cursor()
+resultValue=cur.execute("SELECT*FROM books")
+if resultValue>0:
+    bookDetails =cur.fetchall()
+    return render_template('users.html',bookDetails=bookrDetails)
+        def books():
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
